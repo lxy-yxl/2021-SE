eda5afe0119b49565e272c36e220f0e0a6a99d12
lxy-yxl <chengjing4373@outlook.com>
2021-12-05 15:38:13 +0800

    12-5.2

--- springboot/src/main/java/com/example/demo/common/MybatisPlusConfig.java
+++ springboot/src/main/java/com/example/demo/common/MybatisPlusConfig.java
@@ -7,6 +7,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+//mybatis-plus分页插件
 @Configuration
 @MapperScan("com.example.demo.mapper")
 public class MybatisPlusConfig {
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div style="padding: 10px">
 <!--  功能区域 -->
     <div style="margin: 10px 0">
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary" @click="add">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
@@ -12,9 +12,13 @@
       <el-button type="primary" style="margin-left: 5px">搜索</el-button>
     </div>
     <el-table :data="tableData" border stripe style="width: 100%">
-      <el-table-column prop="date" label="日期" sortable/>
-      <el-table-column prop="name" label="姓名"  />
+      <el-table-column prop="id" label="ID" sortable/>
+      <el-table-column prop="username" label="用户名"  />
+      <el-table-column prop="nickName" label="昵称" />
+      <el-table-column prop="age" label="年龄" />
+      <el-table-column prop="sex" label="性别" />
       <el-table-column prop="address" label="地址" />
+
       <el-table-column fixed="right" label="操作" width="120">
         <template #default="scope">
           <el-button type="text"  @click="handleEdit(scope.row)"
@@ -42,11 +46,44 @@
           @current-change="handleCurrentChange"
       >
       </el-pagination>
+      <el-dialog v-model="dialogVisible" title="Tips" width="30%">
+        <el-form :model="form" label-width="120px">
+          <el-form-item label="用户名">
+            <el-input v-model="form.username" style="width:80%" ></el-input>
+          </el-form-item>
+          <el-form-item label="昵称">
+            <el-input v-model="form.nickName" style="width:80%" ></el-input>
+          </el-form-item>
+          <el-form-item label="年龄">
+            <el-input v-model="form.age" style="width:80%" ></el-input>
+          </el-form-item>
+          <el-form-item label="性别">
+            <div>
+              <el-radio v-model="form.sex" label="男">男</el-radio>
+              <el-radio v-model="form.sex" label="女">女</el-radio>
+              <el-radio v-model="form.sex" label="其他">其他</el-radio>
+            </div>
+          </el-form-item>
+          <el-form-item label="地址">
+          <el-input type="textarea" v-model="form.address" style="width:80%" ></el-input>
+        </el-form-item>
+
+
+        </el-form>
+        <template #footer>
+           <span class="dialog-footer">
+              <el-button @click="dialogVisible = false">Cancel</el-button>
+               <el-button type="primary" @click="save">Confirm</el-button>
+      </span>
+        </template>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
+import request from "../utils/request";
+
 export default {
   name:'Home',
   components:{
@@ -54,34 +91,26 @@ export default {
 },
   data() {
     return {
+       form:{},
+      dialogVisible:false,
       search: '',
       currentPage:1,
       total:10,
       tableData: [
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-04',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-01',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
+
       ],
     }
   },
   methods:{
+    save(){
+        request.post("/api/user",this.form).then(res =>{
+            console.log(res)
+        })
+    },
+    add(){
+        this.dialogVisible=true
+        this.form={}
+    },
     handleEdit(){
 
     },
