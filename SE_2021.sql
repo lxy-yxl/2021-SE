9949b319f390b4d32a71d6e363f033994997f363
lxy-yxl <chengjing4373@outlook.com>
2021-12-13 16:45:55 +0800

    初版数据库的表，无内容，可以下载之后直接导入mysql

---
+++ SE_2021.sql
@@ -0,0 +1,400 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: pigplatform
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `annoucement`
+--
+
+DROP TABLE IF EXISTS `annoucement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `annoucement` (
+  `announcement_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `title` varchar(45) NOT NULL,
+  `content` varchar(1000) NOT NULL,
+  `time` varchar(45) NOT NULL,
+  `status` varchar(45) NOT NULL,
+  PRIMARY KEY (`announcement_id`),
+  UNIQUE KEY `announcement_id_UNIQUE` (`announcement_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='通知表\n';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `annoucement`
+--
+
+LOCK TABLES `annoucement` WRITE;
+/*!40000 ALTER TABLE `annoucement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `annoucement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `comment_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `item_id` varchar(45) DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`comment_id`),
+  UNIQUE KEY `comment_id_UNIQUE` (`comment_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='评论表\n';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fav`
+--
+
+DROP TABLE IF EXISTS `fav`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fav` (
+  `fav_id` int NOT NULL,
+  `object_id` int DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  PRIMARY KEY (`fav_id`),
+  UNIQUE KEY `fav_id_UNIQUE` (`fav_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='关系表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fav`
+--
+
+LOCK TABLES `fav` WRITE;
+/*!40000 ALTER TABLE `fav` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fav` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favourite_cart`
+--
+
+DROP TABLE IF EXISTS `favourite_cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `favourite_cart` (
+  `fav_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `description` varchar(1000) DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `coverimg` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`fav_id`),
+  UNIQUE KEY `fav_id_UNIQUE` (`fav_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favourite_cart`
+--
+
+LOCK TABLES `favourite_cart` WRITE;
+/*!40000 ALTER TABLE `favourite_cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favourite_cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lease_extension`
+--
+
+DROP TABLE IF EXISTS `lease_extension`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lease_extension` (
+  `lease_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL,
+  `day_number` int DEFAULT NULL,
+  `rent_added` int DEFAULT NULL,
+  PRIMARY KEY (`lease_id`),
+  UNIQUE KEY `lease_id_UNIQUE` (`lease_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='延长租赁周期表\n';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lease_extension`
+--
+
+LOCK TABLES `lease_extension` WRITE;
+/*!40000 ALTER TABLE `lease_extension` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lease_extension` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `senderID` int NOT NULL,
+  `receiverID` int NOT NULL,
+  `time` varchar(45) NOT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`senderID`,`receiverID`,`time`),
+  UNIQUE KEY `senderID_UNIQUE` (`senderID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='消息表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `object`
+--
+
+DROP TABLE IF EXISTS `object`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `object` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `deposit` varchar(45) DEFAULT NULL,
+  `tag` varchar(45) DEFAULT NULL,
+  `rent_daliy` int DEFAULT NULL,
+  `new_level` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`product_id`),
+  UNIQUE KEY `product_id_UNIQUE` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `object`
+--
+
+LOCK TABLES `object` WRITE;
+/*!40000 ALTER TABLE `object` DISABLE KEYS */;
+/*!40000 ALTER TABLE `object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+  `oder_id` int NOT NULL AUTO_INCREMENT,
+  `object_id` int NOT NULL,
+  `borrower_id` int DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `rent_tatol` int DEFAULT NULL,
+  `lentout_time` time DEFAULT NULL,
+  `return_time` time DEFAULT NULL,
+  `created_time` time DEFAULT NULL,
+  `finished_time` time DEFAULT NULL,
+  PRIMARY KEY (`oder_id`),
+  KEY `object_id_idx` (`object_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `picture`
+--
+
+DROP TABLE IF EXISTS `picture`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `picture` (
+  `picture_id` int NOT NULL AUTO_INCREMENT,
+  `type` int NOT NULL,
+  `item_id` int DEFAULT NULL,
+  `picture_url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`picture_id`),
+  UNIQUE KEY `picture_id_UNIQUE` (`picture_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='图片表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `picture`
+--
+
+LOCK TABLES `picture` WRITE;
+/*!40000 ALTER TABLE `picture` DISABLE KEYS */;
+/*!40000 ALTER TABLE `picture` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `post` (
+  `post_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `expected_price` int DEFAULT NULL,
+  PRIMARY KEY (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='发帖的表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `post`
+--
+
+LOCK TABLES `post` WRITE;
+/*!40000 ALTER TABLE `post` DISABLE KEYS */;
+/*!40000 ALTER TABLE `post` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `remarks`
+--
+
+DROP TABLE IF EXISTS `remarks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `remarks` (
+  `remark_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `time` time DEFAULT NULL,
+  `content` varchar(1000) DEFAULT NULL,
+  `grade` int DEFAULT NULL,
+  `type` int DEFAULT NULL,
+  `item_id` int DEFAULT NULL,
+  PRIMARY KEY (`remark_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='评价表\n';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `remarks`
+--
+
+LOCK TABLES `remarks` WRITE;
+/*!40000 ALTER TABLE `remarks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `remarks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `report`
+--
+
+DROP TABLE IF EXISTS `report`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `report` (
+  `report_id` int NOT NULL AUTO_INCREMENT,
+  `type` int DEFAULT NULL,
+  `item_id` int DEFAULT NULL,
+  `reason` varchar(45) DEFAULT NULL,
+  `adimin_id` int DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`report_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='报告表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `report`
+--
+
+LOCK TABLES `report` WRITE;
+/*!40000 ALTER TABLE `report` DISABLE KEYS */;
+/*!40000 ALTER TABLE `report` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `nick_name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `avatar` varchar(45) DEFAULT NULL,
+  `gender` varchar(45) DEFAULT NULL,
+  `telphone` int DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `zone` varchar(45) DEFAULT NULL,
+  `register_time` varchar(45) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  `reputation` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `userID_UNIQUE` (`user_id`),
+  UNIQUE KEY `nick_name_UNIQUE` (`nick_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'pigplatform'
+--
+
+--
+-- Dumping routines for database 'pigplatform'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-13 16:43:45
